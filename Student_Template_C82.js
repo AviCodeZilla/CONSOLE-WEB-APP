efc9f9bbaa756b67983144899f59d07a6fd3fefe
Arnav Singhal <aviinfinity10@gmail.com>
2023-09-04 19:49:14 -0500

    Add files via upload

---
+++ Student_Template_C82.js
@@ -0,0 +1,29 @@
+console.log("My first console project");
+
+    var first_name = "Arnav";
+    var last_name = "Singhal";
+    first_name.concat(last_name)
+    var full_name =  ArnavSinghal;
+    console.log(full_name);
+
+    var a = 10;
+    var b = 5;
+    var add = a + b;
+    console.log(add);
+
+    var sub = a - b;
+    console.log(sub);
+/*
+Create a variable named multiple and store the multiplication value of a and b in that variable.
+
+Type: var multiple = a * b
+
+Then, print the variable to display the multiplication value
+
+Type: console.log(multiple)
+*/
+    var multiple = a * b;
+    console.log(multiple);
+
+    var divide = a / b ;
+    console.log(divide);
